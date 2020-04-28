c11489f2120c2b9848198274a07fee284ee4717c
lucascampos <Lucas_Campos@gap.com>
2020-04-28 20:48:25 -0300

    Creating git repo

---
+++ src/main/java/com/gorillahackaton/GorillaHackatonApplication.java
@@ -0,0 +1,13 @@
+package com.gorillahackaton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GorillaHackatonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GorillaHackatonApplication.class, args);
+    }
+
+}
